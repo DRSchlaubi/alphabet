1fb5a2d717cf4d712f6515f9948ec28e7cb58168
Michael Rittmeister <michael@rittmeister.in>
2023-07-12 15:26:01 +0200

    Update dependencies - Bump Gradle to 8.2 - Bump okio to 3.4.0 - Actualize yarn.lock - No longer use deprecated kotlin js plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("js") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("multiplatform") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 group = "dev.schlaubi"
@@ -11,18 +11,22 @@ repositories {
     maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
-dependencies {
-    implementation("dev.kord", "kord-core", "0.10.0-SNAPSHOT")
-    implementation("dev.kord.x:kordx.emoji:feature-mpp-SNAPSHOT")
-    implementation("com.squareup.okio", "okio-nodefilesystem", "3.3.0")
-    implementation("org.jetbrains.kotlin-wrappers", "kotlin-node", "18.16.12-pre.583")
-    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.5.0")
-}
-
 kotlin {
     js(IR) {
         nodejs {
             binaries.executable()
+        }
+    }
+
+    sourceSets {
+        named("jsMain") {
+            dependencies {
+                implementation("dev.kord:kord-core:0.10.0-SNAPSHOT")
+                implementation("dev.kord.x:kordx.emoji:feature-mpp-SNAPSHOT")
+                implementation("com.squareup.okio:okio-nodefilesystem:3.4.0")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-node:18.16.12-pre.594")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+            }
         }
     }
 }
